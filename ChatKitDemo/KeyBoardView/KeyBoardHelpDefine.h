da0161380c9d69b5604b01a5a27ed54936170e1c
余强 <yuqiang@yuqiangdeMacBook-Air.local>
2017-08-25 01:17:58 +0800

    update

--- ChatKitDemo/KeyBoardView/KeyBoardHelpDefine.h
+++ ChatKitDemo/KeyBoardView/KeyBoardHelpDefine.h
@@ -3,7 +3,7 @@
 //  KeyBoardView
 //
 //  Created by 余强 on 16/3/20.
-//  Copyright © 2016年 你好，我是余强，一位来自上海的ios开发者，现就职于bdcluster(上海大数聚科技有限公司)。这个工程致力于完成一个优雅的IM实现方案. All rights reserved.
+
 //
 
 #ifndef ChatHelpDefine_h
